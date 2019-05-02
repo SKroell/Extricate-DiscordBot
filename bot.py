7359843709b95e86b9b17ef96cc4c0b36e573716
Stephano <stefan@stemaki.dk>
2019-05-02 23:56:40 +0200

    Added leaderboard command

--- bot.py
+++ bot.py
@@ -1,90 +1,11 @@
 import os
-import math
 import discord
+from discord.ext import commands
 import asyncio
 import steamlb as sl
-import datetime
-
-icon_url = "https://cdn.discordapp.com/avatars/542096662894608405/c2dbfde314c1f2a12da25ea489a0e98c.webp"
-steam_url = "https://store.steampowered.com/app/1012970/Extricate/"
-
-# Leaderboard Stuff
-steam_lb = sl.SteamLeaderboard(1012970, os.environ['steam_api_key'])
-leaderboards = [
-    3236666, 3236720, 3236721, 3236723, 3236724, 3236725, 3236726, 3236727, 3236728, 3236729,  # Section A
-    3236730, 3236731, 3236732, 3236733, 3236734, 3236735, 3236736, 3236737, 3236738, 3236739,  # Section B
-    3236741, 3236742, 3236743, 3236744, 3236745, 3236746, 3236747, 3236748, 3236749, 3236750,  # Section C
-    3236751, 3236752, 3236753, 3236754, 3236756, 3236757, 3236758, 3236759, 3236760, 3236761,  # Section D
-    3236762, 3236763, 3236764, 3236765, 3236766, 3236767, 3236768, 3236769, 3236770, 3236771,  # Section E
-]
-
-leaderboards_exp = [
-    3272402, 3272403, 3272404, 3272405, 3272406, 3272407, 3272408, 3272409, 3272410, 3272412,  # Section A EXP
-    3272414, 3272416, 3272417, 3272418, 3272419, 3272420, 3272421, 3272422, 3272424, 3272425,  # Section B EXP
-    3272427, 3272428, 3272429, 3272430, 3272431, 3272432, 3272433, 3272435, 3272436, 3272437,  # Section C EXP
-    3272439, 3272440, 3272441, 3272442, 3272443, 3272444, 3272445, 3272446, 3272447, 3272448,  # Section D EXP
-    3272449, 3272450, 3272452, 3272453, 3272454, 3272455, 3272456, 3272457, 3272458, 3272459,  # Section E EXP
-]
-
-# Discord emotes instead of numbers
-lb_index = [
-    ":trophy:",
-    ":second_place:",
-    ":third_place:",
-    ":four:",
-    ":five:",
-    ":six:",
-    ":seven:",
-    ":eight:",
-    ":nine:",
-    ":keycap_ten:",
-]
 
 
-# Calculates scores for all players
-# Scores are calculated with sqrt(n)/sqrt(k/10)
-# Where n is the amount of entries and k is the rank
-def calculate_leaderboard_scores(exp):
-    player_scores = {}
-    leaderboard_ids = leaderboards_exp if exp else leaderboards
-
-    print("Getting leaderboard scores...")
-
-    # Loop trough the leaderboard
-    for lbid in leaderboard_ids:
-        lb = steam_lb.get(lbid=lbid)
-
-        # Skip if leaderboard contains no entries
-        if lb is None:
-            continue
-
-        # Calculate score for player
-        for score in lb.entries:
-            lb_score = math.sqrt(len(lb.entries)) / math.sqrt(score.rank / 10)
-            current_score = player_scores.get(score.steam_id, 0)
-            player_scores[score.steam_id] = current_score + lb_score
-
-    # Return playerscores
-    return player_scores
-
-
-def sort_entries(entries):
-    entries = [(v, k) for k, v in entries.items()]
-    entries.sort()
-    entries.reverse()  # Highest first
-    return [(k, v) for v, k in entries]
-
-
-def create_lb_embed(msg, exp):
-    now = datetime.datetime.now()
-    title = "Leaderboard (EXP)" if exp else "Leaderboard"
-    embed = discord.Embed(title=title, description=msg, color=0xd30106)
-    embed.set_author(name="Extricate Game", url=steam_url, icon_url=icon_url)
-    embed.set_footer(text="Updated: " + now.strftime("%Y-%m-%d %H:%M") + "(UTC)")
-    return embed
-
-
-class ExtricateBot(discord.Client):
+class ExtricateBot(commands.Bot):
     last_msg = None
 
     def __init__(self, *args, **kwargs):
@@ -106,28 +27,40 @@ class ExtricateBot(discord.Client):
             msg = ""
             msg_exp = ""
 
-            stable_scores = sort_entries(calculate_leaderboard_scores(exp=False))
-            exp_scores = sort_entries(calculate_leaderboard_scores(exp=True))
+            stable_scores = sl.sort_entries(sl.calculate_leaderboard_scores(exp=False))
+            exp_scores = sl.sort_entries(sl.calculate_leaderboard_scores(exp=True))
 
             for x in range(10):
                 if len(stable_scores) > x:
                     username = sl.get_username(os.environ['steam_api_key'], stable_scores[x][0])
-                    msg += lb_index[x] + " **" + str(username) + "** [**" + str("%.2f" % stable_scores[x][1]) + "** pts]\n"
+                    msg += sl.lb_index[x] + " **" + str(username) + "** [**" + str("%.2f" % stable_scores[x][1]) + "** pts]\n"
                 if len(exp_scores) > x:
                     username_exp = sl.get_username(os.environ['steam_api_key'], exp_scores[x][0])
-                    msg_exp += lb_index[x] + " **" + str(username_exp) + "** [**" + str("%.2f" % exp_scores[x][1]) + "** pts]\n"
+                    msg_exp += sl.lb_index[x] + " **" + str(username_exp) + "** [**" + str("%.2f" % exp_scores[x][1]) + "** pts]\n"
             msg += "\nPoints are awarded for every leaderboard (not EXP). Refreshed every 15 minutes"
             msg_exp += "\nPoints are awarded for every EXP leaderboard. Refreshed every 15 minutes"
 
             if len(await channel.history(limit=5).flatten()) >= 2:
                 history = await channel.history(limit=5).flatten()
-                await history[1].edit(embed=create_lb_embed(msg, False))
-                await history[0].edit(embed=create_lb_embed(msg_exp, True))
-                print("EDITED")
+                await history[1].edit(embed=sl.create_lb_embed(msg, "Leaderboard"))
+                await history[0].edit(embed=sl.create_lb_embed(msg_exp, "Leaderboard (EXP)"))
+                print("Leaderboard Updated!")
             else:
-                await channel.send(embed=create_lb_embed(msg_exp, True))
+                await channel.send(embed=sl.create_lb_embed(msg_exp, "Leaderboard (EXP)"))
             await asyncio.sleep(900)  # task runs every 15 minutes (roughly since we dont count for running time)
 
+    # Commands
+    @commands.command()
+    async def leaderboard(ctx, lb):
+        # Get leaderboard for specific leaderboard
+        times = sl.sort_entries(sl.get_leaderboard_times(lb, 'EXP' in lb), False)
+        msg = ""
+        for x in range(10):
+            username = sl.get_username(os.environ['steam_api_key'], times[x][0])
+            msg += sl.lb_index[x] + " **" + str(username) + "** [**" + sl.convert_to_nicetime(times[x][1]) + "**]\n"
+        await ctx.send(embed=sl.create_lb_embed(msg, "Leaderboard {}".format(lb)))
 
-client = ExtricateBot()
+
+client = ExtricateBot(command_prefix='?', description="ExtricateBot")
+client.add_command(client.leaderboard)
 client.run(os.environ['discord_auth'])
--- steamlb.py
+++ steamlb.py
@@ -1,6 +1,115 @@
 import requests
 from bs4 import BeautifulSoup
 import typing
+import discord
+import datetime
+import math
+import os
+
+icon_url = "https://cdn.discordapp.com/avatars/542096662894608405/c2dbfde314c1f2a12da25ea489a0e98c.webp"
+steam_url = "https://store.steampowered.com/app/1012970/Extricate/"
+
+# Leaderboard Stuff
+leaderboards = {
+    'A1': 3236666, 'A2': 3236720, 'A3': 3236721, 'A4': 3236723, 'A5': 3236724, 'A6': 3236725, 'A7': 3236726, 'A8': 3236727, 'A9': 3236728, 'A10': 3236729, # Section A
+    'B1': 3236730, 'B2': 3236731, 'B3': 3236732, 'B4': 3236733, 'B5': 3236734, 'B6': 3236735, 'B7': 3236736, 'B8': 3236737, 'B9': 3236738, 'B10': 3236739,  # Section B
+    'C1': 3236741, 'C2': 3236742, 'C3': 3236743, 'C4': 3236744, 'C5': 3236745, 'C6': 3236746, 'C7': 3236747, 'C8': 3236748, 'C9': 3236749, 'C10': 3236750,  # Section C
+    'D1': 3236751, 'D2': 3236752, 'D3': 3236753, 'D4': 3236754, 'D5': 3236756, 'D6': 3236757, 'D7': 3236758, 'D8': 3236759, 'D9': 3236760, 'D10': 3236761,  # Section D
+    'E1': 3236762, 'E2': 3236763, 'E3': 3236764, 'E4': 3236765, 'E5': 3236766, 'E6': 3236767, 'E7': 3236768, 'E8': 3236769, 'E9': 3236770, 'E10': 3236771,  # Section E
+}
+
+leaderboards_exp = {
+    'A1_EXP': 3272402, 'A2_EXP': 3272403, 'A3_EXP': 3272404, 'A4_EXP': 3272405, 'A5_EXP': 3272406, 'A6_EXP': 3272407, 'A7_EXP': 3272408, 'A8_EXP': 3272409, 'A9_EXP': 3272410, 'A10_EXP': 3272412,  # Section A EXP
+    'B1_EXP': 3272414, 'B2_EXP': 3272416, 'B3_EXP': 3272417, 'B4_EXP': 3272418, 'B5_EXP': 3272419, 'B6_EXP': 3272420, 'B7_EXP': 3272421, 'B8_EXP': 3272422, 'B9_EXP': 3272424, 'B10_EXP': 3272425,  # Section B EXP
+    'C1_EXP': 3272427, 'C2_EXP': 3272428, 'C3_EXP': 3272429, 'C4_EXP': 3272430, 'C5_EXP': 3272431, 'C6_EXP': 3272432, 'C7_EXP': 3272433, 'C8_EXP': 3272435, 'C9_EXP': 3272436, 'C10_EXP': 3272437,  # Section C EXP
+    'D1_EXP': 3272439, 'D2_EXP': 3272440, 'D3_EXP': 3272441, 'D4_EXP': 3272442, 'D5_EXP': 3272443, 'D6_EXP': 3272444, 'D7_EXP': 3272445, 'D8_EXP': 3272446, 'D9_EXP': 3272447, 'D10_EXP': 3272448,  # Section D EXP
+    'E1_EXP': 3272449, 'E2_EXP': 3272450, 'E3_EXP': 3272452, 'E4_EXP': 3272453, 'E5_EXP': 3272454, 'E6_EXP': 3272455, 'E7_EXP': 3272456, 'E8_EXP': 3272457, 'E9_EXP': 3272458, 'E10_EXP': 3272459,  # Section E EXP
+}
+
+# Discord emotes instead of numbers
+lb_index = [
+    ":trophy:",
+    ":second_place:",
+    ":third_place:",
+    ":four:",
+    ":five:",
+    ":six:",
+    ":seven:",
+    ":eight:",
+    ":nine:",
+    ":keycap_ten:",
+]
+
+
+def convert_to_nicetime(time):
+    seconds = time * 0.001
+    minutes = int(seconds / 60)
+    seconds_rest = seconds % 60
+    return '{:02}:{:06.3f}'.format(minutes, seconds_rest)
+
+
+# Get times for specific leaderboard
+def get_leaderboard_times(lb, exp=False):
+    player_times = {}
+    leaderboard_id = leaderboards_exp[lb] if exp else leaderboards[lb]
+
+    print("Getting leaderboard scores for " + lb + "...")
+
+    lb = steam_lb.get(lbid=leaderboard_id)
+
+    # Skip if leaderboard contains no entries
+    if lb is None:
+        return
+
+    # Calculate score for player
+    for entry in lb.entries:
+        player_times[entry.steam_id] = entry.score
+
+    # Return playerscores
+    return player_times
+
+
+# Calculates scores for all players
+# Scores are calculated with sqrt(n)/sqrt(k/10)
+# Where n is the amount of entries and k is the rank
+def calculate_leaderboard_scores(exp):
+    player_scores = {}
+    leaderboard_ids = leaderboards_exp if exp else leaderboards
+
+    print("Getting leaderboard scores...")
+
+    # Loop trough the leaderboard
+    for lbid in leaderboard_ids.values():
+        lb = steam_lb.get(lbid=lbid)
+
+        # Skip if leaderboard contains no entries
+        if lb is None:
+            continue
+
+        # Calculate score for player
+        for score in lb.entries:
+            lb_score = math.sqrt(len(lb.entries)) / math.sqrt(score.rank / 10)
+            current_score = player_scores.get(score.steam_id, 0)
+            player_scores[score.steam_id] = current_score + lb_score
+
+    # Return playerscores
+    return player_scores
+
+
+def sort_entries(entries, high_first=True):
+    entries = [(v, k) for k, v in entries.items()]
+    entries.sort()
+    if high_first:
+        entries.reverse()  # Highest first
+    return [(k, v) for v, k in entries]
+
+
+def create_lb_embed(msg, title):
+    now = datetime.datetime.now()
+    embed = discord.Embed(title=title, description=msg, color=0xd30106)
+    embed.set_author(name="Extricate Game", url=steam_url, icon_url=icon_url)
+    embed.set_footer(text="Updated: " + now.strftime("%Y-%m-%d %H:%M") + "(UTC)")
+    return embed
 
 
 # Get steam username from SteamID
@@ -71,3 +180,6 @@ class Entry:
 
     def __repr__(self):
         return f"<Entry #{self.rank} {self.steam_id}: {self.score} pts>"
+
+
+steam_lb = SteamLeaderboard(1012970, os.environ['steam_api_key'])
